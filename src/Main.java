a94d63eee629b05708033252155d2636ac096cc0
Jhonatan <jhoncruz.duarte10@gmail.com>
2026-04-13 16:12:24 -0300

    fiz o adicionar tarefa

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,33 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> tarefas = new ArrayList<>();
+        Scanner in = new Scanner(System.in);
+
+        int opcao;
+
+        do {
+            System.out.println("\n1 - Adicionar tarefa");
+            System.out.println("2 - Listar tarefas");
+            System.out.println("3 - Remover Tarefa");
+            System.out.println("0 - sair");
+            System.out.print("Escolha uma opção: ");
+
+            opcao = in.nextInt();
+            in.nextLine(); // Limpar buffer
+
+            switch(opcao){
+                case 1:
+                    System.out.print("\nDigite a tarefa: ");
+                    String tarefa = in.nextLine();
+                    tarefas.add(tarefa);
+                    System.out.println("Tarefa cadastrada!");
+                    break;
+            }
+        } while (opcao != 0);
+
+        in.close();
+    }
 }
